3f38ced26b8e3daa4afd325c7a221f335670ea80
Naz <nazmalik08@gmail.com>
2017-04-05 11:08:17 +0100

    add tag model

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,8 +9,14 @@ class Link
   property :title, String
   property :url, String
 
+  # has n, :tags, :through => Resource
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL']||"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+# tag = Tag.create
+# link = Link.create
+
+
