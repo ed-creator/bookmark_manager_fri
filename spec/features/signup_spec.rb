d2ebe78345b095f60fffffb27ed6bb5f67058d9a
Edward Ward <edwardwardward@gmail.com>
2017-04-06 15:00:37 +0100

    Add valid  email checks client side, model & database

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -20,4 +20,23 @@ feature 'allows user to sign up with an email & password' do
     expect(page).to have_content 'Passwords do not match!'
     expect(User.count).to eq 0
   end
+
+  scenario 'do not enter email' do
+    visit '/users/new'
+    fill_in 'password', with: 's3cr3t'
+    fill_in 'password_confirmation', with: 's3cr3t'
+    expect(current_path).to eq '/users/new'
+    click_button 'Sign Up'
+    expect(User.count).to eq 0
+  end
+
+  scenario 'enter invalid email' do
+    visit '/users/new'
+    fill_in 'email', with: 'hi'
+    fill_in 'password', with: 's3cr3t'
+    fill_in 'password_confirmation', with: 's3cr3t'
+    expect(current_path).to eq '/users/new'
+    click_button 'Sign Up'
+    expect(User.count).to eq 0
+  end
 end
